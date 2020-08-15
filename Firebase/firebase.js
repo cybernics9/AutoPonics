8815cedd27c1fcbd5d6ba046142bbcba51ba78aa
Sajini Ishanthi <dsajini.i@gmail.com>
2020-08-15 09:16:05 +0530

    Firebase connection
    Connected the firebase Database to theReact App.

---
+++ Firebase/firebase.js
@@ -0,0 +1,48 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  
+};
+
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+    this.db = app.database();
+  }
+
+  // *** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+
+  user = uid => this.db.ref(`users/${uid}`);
+
+  users = () => this.db.ref('users');
+}
+
+export default Firebase;
